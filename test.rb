cb24e6545943b9cad6fa5c601a3918d61a88a781
Amy Donnelly <akdonnelly@gmail.com>
2019-09-10 13:08:22 +0000

    Initial commit

--- test.rb
+++ test.rb
@@ -1,1 +1,32 @@
-puts 'Hi Amy'
+images = ([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+
+class Test 
+
+
+def initialize(picture)
+   @images = picture
+end
+
+def run
+    @images.each do |data|
+      puts data.join
+    end
+
+end
+
+img = Test.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+img.run
+
+end
